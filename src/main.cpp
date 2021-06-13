62e7e3fc06532260b15de3f90f3209fafeebaa5e
Alton-Y <alton.yeung@ryerson.ca>
2021-06-12 23:57:25 -0400

    parse serial input

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 ADG2188 adg2188;
 bool state = true;
 
+int x = 0;
+int y = 0;
+
 void setup()
 {
   Serial.begin(115200);
@@ -14,16 +17,44 @@ void setup()
 
 void loop()
 {
-  
-  for (uint8_t x = 0; x < 8; x++)
-  {
-    for (uint8_t y = 0; y < 8; y++)
-    {
-      adg2188.set(state, x, y, true);
-      adg2188.printState();
-      delay(300);
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    switch (inByte) {
+      case 'x':
+        x = Serial.parseInt();
+        break;
+      case 'y':
+        y = Serial.parseInt();
+        break;
+      case 't':
+        state = true;
+        break;
+      case 'f':
+        state = false;
+      case '\n':
+        adg2188.set(state, x, y, true);
+        Serial.print("X");
+        Serial.print(x);
+        Serial.print(" Y");
+        Serial.print(y);
+        Serial.print(" ");
+        Serial.println(state);
+        adg2188.printState();
+        break;
     }
   }
 
-  state = !state;
+  // for (uint8_t x = 0; x < 8; x++)
+  // {
+  //   for (uint8_t y = 0; y < 8; y++)
+  //   {
+  //     adg2188.set(state, x, y, true);
+  //     adg2188.printState();
+  //     delay(300);
+  //   }
+  // }
+
+
+
+  // state = !state;
 }
